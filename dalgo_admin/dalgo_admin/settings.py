5647aef1dd9261a099ed2ebece97cade0d567241
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2024-01-24 21:32:43 +0530

    bug fixed

--- dalgo_admin/dalgo_admin/settings.py
+++ dalgo_admin/dalgo_admin/settings.py
@@ -147,3 +147,4 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
