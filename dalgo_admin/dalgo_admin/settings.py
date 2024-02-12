066c43c5a94ad1439827ddce1503a1e4be47767a
Aman Khan <amankhancompq2002@gmail.com>
2024-02-12 18:58:35 +0530

    Add Toolbar

--- dalgo_admin/dalgo_admin/settings.py
+++ dalgo_admin/dalgo_admin/settings.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+from dotenv import main
 from pathlib import Path
 
-load_dotenv()
+main.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
